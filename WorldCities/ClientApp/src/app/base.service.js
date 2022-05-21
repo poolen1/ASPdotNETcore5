886c3941da09ba6808c072ee23fc6801ec5dd15d
Nick Poole <42500386+poolen1@users.noreply.github.com>
2022-05-21 11:51:24 -0700

    Added base service and city service files.

---
+++ WorldCities/ClientApp/src/app/base.service.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var BaseService = /** @class */ (function () {
+    function BaseService(http, baseUrl) {
+        this.http = http;
+        this.baseUrl = baseUrl;
+    }
+    return BaseService;
+}());
+exports.BaseService = BaseService;
+//# sourceMappingURL=base.service.js.map
